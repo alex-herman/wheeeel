fc7e3fbba887989008c11afe5aaf52eae4ee788f
alex-herman <herman.alex10@gmail.com>
2023-11-25 11:37:51 -0700

    sorta spinning

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,17 @@
 function spin(){
-	console.log("hi")
+	let tasks = document.getElementsByClassName("textbox");
+	setTimeout(tick(0, 100), 100);
+}
+
+let tasks = document.getElementsByClassName("textbox");
+function tick(i, counter){
+	console.log(i)
+	if(counter < 2000){
+		if(i == tasks.length) { i = 0 }
+		tasks[i].style.backgroundColor = "red"
+		let prevI = i-1 
+		if(i == 0){ prevI = tasks.length - 1}
+		tasks[prevI].style.backgroundColor = "rgba(255,255,255,.33)"
+		setTimeout(tick(i+1, counter*1.01))
+	}
 }
