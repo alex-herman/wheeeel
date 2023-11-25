1ddf18f5f57ef8ced3148f69bdb3fdca366492ac
alex-herman <herman.alex10@gmail.com>
2023-11-25 14:31:11 -0700

    more natural feeling spin cadence

--- static/js/main.js
+++ static/js/main.js
@@ -3,16 +3,27 @@ const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
 let tasks = document.getElementsByClassName("textbox");
 const spin = async () => {
-	let sleepTime = 100
+	for(let i = 0; i < tasks.length; i++){
+		tasks[i].style.backgroundColor = "rgba(255,255,255,.33)"
+	}
+	
+	let sleepTime = 0
 	let i = 0
-	while(sleepTime <  2500){
+	let ti = 0
+	while(sleepTime <  250){
 		if(i == tasks.length){ i = 0 }
 		let prevI = i - 1
 		if(i == 0){ prevI = tasks.length - 1 }
 		await sleep(sleepTime)
 		tasks[i].style.backgroundColor = "red"
 		tasks[prevI].style.backgroundColor = "rgba(255,255,255,.33)"
-		sleepTime = sleepTime * 1.15
+		sleepTime = Math.pow(2, ti * (1/10))
 		i = i + 1
+		ti = ti + 1
 	}
+	//make the spin button clickable again at the end of this function
+}
+
+function holdSpinButton(){
+	
 }
